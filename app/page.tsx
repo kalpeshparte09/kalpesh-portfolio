5fde712b32e7cdde213e4f2c7c08080a617d228c
kalpeshparte09 <130503636+kalpeshparte09@users.noreply.github.com>
2025-09-23 17:17:34 -0400

    Update portfolio code for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Contact from "@/components/Contact";
-import Chatbot from "@/components/Chatbot";
 import LetterGlitch from "@/components/LetterGlitch";
 import Introduction from "@/components/Introduction";
 
